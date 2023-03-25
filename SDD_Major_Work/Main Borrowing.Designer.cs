e6605b0eb138d9dd2c8cde2b23406544966d0fd0
jennica-wang <jennica.wang@education.nsw.gov.au>
2023-03-25 14:07:13 +1100

    initial

--- SDD_Major_Work/Main Borrowing.Designer.cs
+++ SDD_Major_Work/Main Borrowing.Designer.cs
@@ -135,7 +135,7 @@
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(200, 221);
+            this.listBox1.Location = new System.Drawing.Point(210, 240);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(251, 173);
             this.listBox1.TabIndex = 10;
